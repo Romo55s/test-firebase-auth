f36bc245a9a9e69e29d25b3217492a75309175cd
Tony <pp.tony777@gmail.com>
2023-05-28 15:35:05 -0600

    Just code rearrangement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: false,
   firebaseConfig: {
     apiKey: "AIzaSyBkyYzflxqwTaYAjWVhu7i_4AauN0ZS8Mg",
-  authDomain: "reservaciones-adf27.firebaseapp.com",
-  projectId: "reservaciones-adf27",
-  storageBucket: "reservaciones-adf27.appspot.com",
-  messagingSenderId: "268825628920",
-  appId: "1:268825628920:web:7312e147059ed23f3f6b0d",
-  measurementId: "G-FVPDBQF2FZ"
+    authDomain: "reservaciones-adf27.firebaseapp.com",
+    projectId: "reservaciones-adf27",
+    storageBucket: "reservaciones-adf27.appspot.com",
+    messagingSenderId: "268825628920",
+    appId: "1:268825628920:web:7312e147059ed23f3f6b0d",
+    measurementId: "G-FVPDBQF2FZ"
   }
 };
